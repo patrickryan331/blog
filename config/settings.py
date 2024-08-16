c5b375f87749a80107e214d0f95ffe7f78997f7f
patrickryan331 <patrickryan331@gmail.com>
2024-08-15 20:06:24 -0400

    112 class 4 - missing css / admin status / signup not working/ no mini challenge yet

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,8 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
